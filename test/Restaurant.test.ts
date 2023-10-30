a95310dfac733711eec96f85e93c750d3fae665f
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-10-30 14:18:21 +0100

    add test for listAllOrders method

--- test/Restaurant.test.ts
+++ test/Restaurant.test.ts
@@ -14,3 +14,27 @@ describe('Restaurant calculateIncome method', () => {
         expect(totalIncome).toBe(25);
     });
 });
+
+describe('Restaurant listAllOrders method', () => {
+    const restaurant = new Restaurant('Test Restaurant');
+    const order1 = new OrderItem(10);
+    const order2 = new OrderItem(15);
+
+    restaurant.addOrder(order1);
+    restaurant.addOrder(order2);
+
+    // Mock console.log
+    const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
+
+    test('should list all orders', () => {
+        restaurant.listAllOrders();
+        expect(consoleLogSpy).toHaveBeenCalledWith('All Orders:');
+        expect(consoleLogSpy).toHaveBeenCalledWith('Order 1: $10');
+        expect(consoleLogSpy).toHaveBeenCalledWith('Order 2: $15');
+    });
+
+    // Restore console.log
+    afterAll(() => {
+        consoleLogSpy.mockRestore();
+    });
+});
